e51155e078d8cc58a719098c2df9df3c7c862fd2
Edith Cruz McHugh <edith@Ediths-MacBook.local>
2018-02-26 22:54:22 -0800

    learning function

---
+++ js/scripts29.js
@@ -0,0 +1,37 @@
+
+
+function add(x, y) {
+   num = x + y;
+  return num
+}
+var text = add(3,4)
+console.log(text);
+
+
+
+function product(a,b) {
+  num = a * b;
+  return num
+}
+var dec = product(15, 9)
+console.log(dec);
+
+
+
+
+function ages(Andrea,Martina){
+  var age = Andrea - Martina
+  return age
+}
+var realage = ages(49,40)
+  console.log(realage);
+
+
+
+
+function number(num29,num15,num5) {
+  numbers = num29 * num15 * num5
+  return numbers
+}
+var multiply = number(10,3,6)
+console.log(multiply);
